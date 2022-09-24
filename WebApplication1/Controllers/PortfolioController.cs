2be90b583572487da1ccaef2934f2915039c1b37
Fahim Haider <20-42087-1@student.aiub.edu>
2022-09-24 22:34:28 +0600

    LabTask1

---
+++ WebApplication1/Controllers/PortfolioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Scripts/script.js
@@ -0,0 +1,7 @@
+﻿var splide = new Splide(".splide", {
+    type: "loop",
+    height: "80vh",
+    focus: "center",
+    autoWidth: true,
+});
+splide.mount();
